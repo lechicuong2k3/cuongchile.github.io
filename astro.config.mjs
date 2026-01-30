081dcede709b97881ecc347768e6c9a02057a437
lechicuong2k3 <lechicuongldk@gmail.com>
2026-01-30 08:52:26 +0000

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lechicuong2k3.github.io',
+  base: '/cuongchile.github.io/',
   output: 'static',
   integrations: [
     mdx(), 
